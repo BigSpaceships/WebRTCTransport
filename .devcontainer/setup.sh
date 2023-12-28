9ad3d4a6ed5bcf9973d11b5d9b247c671ac43a5a
BigSpaceships <bigspaceships56@gmail.com>
2023-12-28 21:37:52 +0000

    FEAT: initial building stuff

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,1 +1,10 @@
+apt-get update
+apt-get install -y \
+    pkg-config
 
+## Install rustup and common components
+curl https://sh.rustup.rs -sSf | sh -s -- -y 
+
+source "$HOME/.cargo/env"
+
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
---
+++ www/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./bootstrap.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bootstrap.js",
+    },
+    mode: "development",
+    devServer: {
+        static: './dist',
+    },
+    experiments: {
+        asyncWebAssembly: true
+    },
+};
